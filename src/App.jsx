9efc19410b44a5eb8115c8eb91d730c8c3574781
rushabh782 <rushabhpatel877@gmail.com>
2024-01-15 19:18:40 +0530

    moved NEW_GAME state outside of App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,15 @@ import StatusMessage from './components/StatusMessage';
 import History from './components/History';
 import { calculateWinner } from './winner';
 
+const NEW_GAME = [{ squares: Array(9).fill(null), isXNext: false }];
+
 function App() {
-  const [history, setHistory] = useState([
-    { squares: Array(9).fill(null), isXNext: false },
-  ]);
+  const [history, setHistory] = useState(NEW_GAME);
   const [currentMove, setCurrentMove] = useState(0);
 
   const gamingBoard = history[currentMove];
 
   const winner = calculateWinner(gamingBoard.squares);
-
 
   const handleSquareClick = clickedPosition => {
     //null,'X','0'
@@ -26,7 +25,9 @@ function App() {
     setHistory(currentHistory => {
       const isTraversing = currentMove + 1 !== currentHistory.length;
 
-      const lastGamingState = isTraversing ? currentHistory[currentMove] : history[history.length - 1];
+      const lastGamingState = isTraversing
+        ? currentHistory[currentMove]
+        : history[history.length - 1];
 
       const nextSquaresState = lastGamingState.squares.map(
         (squareValue, position) => {
@@ -37,20 +38,22 @@ function App() {
         }
       );
 
-      const base = isTraversing ? currentHistory.slice(0,currentHistory.indexOf(lastGamingState) + 1) : currentHistory;
+      const base = isTraversing
+        ? currentHistory.slice(0, currentHistory.indexOf(lastGamingState) + 1)
+        : currentHistory;
 
       return base.concat({
         squares: nextSquaresState,
         isXNext: !lastGamingState.isXNext,
       });
     });
-    
+
     setCurrentMove(move => move + 1);
   };
 
-  const moveTo = (move) =>{
+  const moveTo = move => {
     setCurrentMove(move);
-  }
+  };
 
   return (
     <div className="app">
