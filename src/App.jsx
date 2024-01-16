aad32145d6ab5cbf455de9086fb30f5d0389616d
rushabh782 <rushabhpatel877@gmail.com>
2024-01-16 22:06:48 +0530

    added background circles

--- src/App.jsx
+++ src/App.jsx
@@ -89,6 +89,8 @@ function App() {
         Current game history
       </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+
+      <div className='bg-balls'/>
     </div>
   );
 }
